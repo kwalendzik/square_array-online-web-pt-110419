7f845ce7914a34634965982d10f0913c83f9f3a0
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-12 02:33:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,7 @@
-#def square_array(array)
-  #squared_array = []
-  #array.each do |i|
-    #squared_array << i ** 2
-  #end
-  #squared_array
-#end
-
 def square_array(array)
   squared_array = []
-  squared_array << array.collect { |x| x**2 }
+  array.each do |i|
+    squared_array << i ** 2
+  end
   squared_array
 end
